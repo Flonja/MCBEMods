ae2535a5969ad4e010c71e1cdfff73d454e7bff7
Flonja <vanjavanessen@icloud.com>
2019-12-31 00:13:54 +0100

    Update it to a newer example

--- mods/ExampleMod/main.js
+++ mods/ExampleMod/main.js
@@ -1,8 +1,4 @@
-var ClientSidedTextPacket = requirePacket("client", "TextPacket");
-var ServerSidedTextPacket = requirePacket("server", "TextPacket");
-
-var textPk = new ClientSidedTextPacket();
-textPk.setMessage("f");
+var modalForm = new ModalFormWindow("Testing", "Choose!", "yes", "no");
 
 /*
 Equivalent of:
@@ -14,23 +10,48 @@ client.on("packet", function(pk) {
 });
 */
 client.on("connected", function() {
-	client.sendPacket(textPk);
+	client.sendMessage("Helloe and welcome to the MCBEMods proxy");
+	modalForm.setTitle(modalForm.getTitle() + client.getAuthData().getDisplayName());
+	client.showWindow(modalForm, 99990);
 });
+
+log.info(TransferPacket);
 
 client.on("packet", function(pk) {
 	if(pk instanceof SetLocalPlayerAsInitializedPacket) {
-		// do something
-		server.sendMessage("Hello, I'm using a example mod");
+	    log.info("sent 'hello' message");
+		server.sendMessage("Hello");
 	}
 	return true;
 });
 
 client.on("command", function(command, args) {
-	client.sendMessage(command);
+    if(command == "test") {
+        client.sendMessage("Testing");
+        return false;
+    }else if(command == "modal") {
+        client.showWindow(modalForm, 99990);
+        return false;
+    }
 	return true;
 });
 
 server.on("message", function(message) {
-	client.sendMessage(message);
-	return false;
+    log.info("[CHAT]: " + message);
+	//client.sendMessage(message);
+	return true;
+});
+
+// FORMS
+client.on("formResponse", function(id, response) {
+    if(response != null) {
+        if(id == 99990) {
+            client.sendMessage(response);
+
+            modalForm.setResponse(response);
+            client.sendMessage(modalForm.getResponse().getClickedButtonText());
+            return false;
+        }
+    }
+	return true;
 });
